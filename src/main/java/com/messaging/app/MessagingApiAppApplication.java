8a734237e52ba23811fca47e49a9d439a3218718
sherg <vikram7ce@gmail.com>
2024-07-12 15:04:16 +0530

    third commit

--- src/main/java/com/messaging/app/MessagingApiAppApplication.java
+++ src/main/java/com/messaging/app/MessagingApiAppApplication.java
@@ -11,7 +11,7 @@ public class MessagingApiAppApplication {
 	
 	@GetMapping("message")
 	public String printGreetings() {
-		return "Hi Oracle";
+		return "SHERYL SHARON G";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApiAppApplication.class, args);
